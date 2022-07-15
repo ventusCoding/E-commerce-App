af593516ef3a2269b4a98b886e850941179fa174
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-07-15 21:52:32 +0100

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+     <h1>Welcome To ProShop</h1>
+    </div>
+  )
+}
+
+export default App
