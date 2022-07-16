c0209392fb19684e9917b1e937978d3c1318677c
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-07-16 16:47:43 +0100

    add unhandledRejection & uncaughtException

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNCAUGHT EXEPTION! 💥💥 Shutting Down... 💥💥");
+  process.exit(1);
+});
+
 mongoose
   .connect(process.env.DATABASE_LOCAL, {
     useNewUrlParser: true,
@@ -13,7 +19,14 @@ mongoose
   });
 
 const PORT = process.env.PORT || 5000;
-
 server.listen(PORT, () => {
   console.log(`Server started in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLER REJECTION! 💥💥 Shutting Down... 💥💥");
+  server.close(() => {
+    process.exit(1);
+  });
 });
