a0cc3b9c546d49151e75094e2498406095e5c9f7
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-07-16 16:41:22 +0100

    add error handling / API Features / catch async & fetch products from database

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const http = require('http');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const productRoutes = require("./routes/productRoutes");
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,5 +20,11 @@ app.get("/", (req, res) => {
 app.use(express.static(`${__dirname}/public`));
 
 app.use("/api/v1/products", productRoutes);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = { server };
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,16 +1,44 @@
+const Product = require("../models/productModel");
+const APIFeatures = require("../utils/apiFeatures");
+const catchasync = require("../utils/catchAsync");
+const AppError = require('../utils/appError');
 
+exports.getProducts = catchasync(async (req, res, next) => {
+  const features = new APIFeatures(Product.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
+  const products = await features.query;
 
-
-const products = require("../data/products");
-
-
-exports.getProducts = (req, res) => {
-    res.json(products);
-}
-
-
-exports.getProductById = (req, res) => {
-    const product = products.find(product => product._id === req.params.id);
-    res.json(product);
+  if (products.length > 0) {
+    res.status(200).json({
+      status: "success",
+      requestedAt: req.requestTime,
+      results: products.length,
+      data: {
+        products,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      results: 0,
+      data: {
+        products,
+      },
+      message: "No Data Found!",
+    });
   }
+});
+
+exports.getProductById = catchasync(async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return next(new AppError("No product found with that ID", 404));
+  }
+
+  res.status(200).json({ status: "success", data: { product } });
+});
