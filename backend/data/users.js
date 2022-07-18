95e009c7de54aefd7c09b816a68d1da9d7a9838d
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-07-18 17:53:48 +0100

    fix data

--- backend/data/users.js
+++ backend/data/users.js
@@ -6,21 +6,26 @@ const users = [
   {
     name: "Admin User",
     email: "admin@example.com",
+    verified: true,
     password: password,
     passwordConfirm: password,
-    isAdmin: true,
+    role: "admin",
   },
   {
     name: "John Doe",
     email: "john@example.com",
+    verified: true,
     password: password,
     passwordConfirm: password,
+    role: "user",
   },
   {
     name: "Jane Doe",
     email: "jane@example.com",
+    verified: true,
     password: password,
     passwordConfirm: password,
+    role: "user",
   },
 ];
 
