7da53c3cca6458b1c5dcc17f7ba1d8d1ff3ff677
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-07-19 17:44:40 +0100

    signup

--- frontend/src/components/RegisterScreen.jsx
+++ frontend/src/components/RegisterScreen.jsx
@@ -1,79 +1,95 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col } from 'react-bootstrap'
-import { useDispatch, useSelector } from 'react-redux'
-import Message from '../components/Message'
-import Loader from '../components/Loader'
-import FormContainer from '../components/FormContainer'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Form, Button, Row, Col } from "react-bootstrap";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import FormContainer from "../components/FormContainer";
+import { useSelector, useDispatch } from "react-redux";
+import { bindActionCreators } from "redux";
+import * as actionCreators from "../store/actions/index";
+import { useForm } from "react-hook-form";
 
 const RegisterScreen = () => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const { register, handleSubmit } = useForm();
+
+  const { loading, error,message } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  const { signup } = bindActionCreators(actionCreators, dispatch);
 
   return (
-       <FormContainer>
+    <FormContainer>
       <h1>Sign Up</h1>
-      {/* {message && <Message variant='danger'>{message}</Message>} */}
-      {/* {error && <Message variant='danger'>{error}</Message>} */}
-      {/* {loading && <Loader />} */}
-      <Form >
-        <Form.Group controlId='name'>
+      {message && <Message variant='success'>{message}</Message>}
+      {error && <Message variant='danger'>{error}</Message>}
+      {loading && <Loader />}
+      <Form onSubmit={handleSubmit(signup)}>
+        <Form.Group controlId="name">
           <Form.Label>Name</Form.Label>
           <Form.Control
-            type='name'
-            placeholder='Enter name'
+            {...register("name", { required: true })}
+            type="name"
+            placeholder="Enter name"
             value={name}
+            required
             onChange={(e) => setName(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group style={{ marginTop: 20 }} controlId='email'>
+        <Form.Group style={{ marginTop: 20 }} controlId="email">
           <Form.Label>Email Address</Form.Label>
           <Form.Control
-            type='email'
-            placeholder='Enter email'
+            {...register("email", { required: true })}
+            type="email"
+            placeholder="Enter email"
             value={email}
+            required
             onChange={(e) => setEmail(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group style={{ marginTop: 20 }} controlId='password'>
+        <Form.Group style={{ marginTop: 20 }} controlId="password">
           <Form.Label>Password</Form.Label>
           <Form.Control
-            type='password'
-            placeholder='Enter password'
+            {...register("password", { required: true })}
+            type="password"
+            placeholder="Enter password"
             value={password}
+            required
             onChange={(e) => setPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Form.Group style={{ marginTop: 20 }} controlId='confirmPassword'>
+        <Form.Group style={{ marginTop: 20 }} controlId="confirmPassword">
           <Form.Label>Confirm Password</Form.Label>
           <Form.Control
-            type='password'
-            placeholder='Confirm password'
+            {...register("passwordConfirm", { required: true })}
+            type="password"
+            placeholder="Confirm password"
             value={confirmPassword}
+            required
             onChange={(e) => setConfirmPassword(e.target.value)}
           ></Form.Control>
         </Form.Group>
 
-        <Button type='submit' variant='primary'>
+        <Button style={{ marginTop: 20 }} type="submit" variant="primary">
           Register
         </Button>
       </Form>
 
-      <Row className='py-3'>
+      <Row className="py-3">
         <Col>
-          Have an Account?{' '}
-          <Link to='/login'>
-            Login
-          </Link>
+          Have an Account? <Link to="/login">Login</Link>
         </Col>
       </Row>
     </FormContainer>
-  )
-}
+  );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
