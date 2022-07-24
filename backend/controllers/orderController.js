9e54d367032ddc580e87e597b147ed817ee92918
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-07-24 15:03:26 +0100

    add createOrder controller

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,10 +3,40 @@ const APIFeatures = require("../utils/apiFeatures");
 const catchasync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-exports.getAllOrders = catchasync(async (req, res, next) => {
-  
-});
+exports.getAllOrders = catchasync(async (req, res, next) => {});
 
-exports.getOrderById = catchasync(async (req, res, next) => {
- 
+exports.getOrderById = catchasync(async (req, res, next) => {});
+
+exports.createOrder = catchasync(async (req, res, next) => {
+  const {
+    orderItems,
+    shippingAdress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+  } = req.body;
+
+  if (orderItems && orderItems.length === 0) {
+    return next(new AppError("No order items found", 400));
+  }
+
+  const order = new Order({
+    orderItems,
+    shippingAdress,
+    paymentMethod,
+    itemsPrice,
+    taxPrice,
+    shippingPrice,
+    totalPrice,
+    user: req.user._id,
+  });
+
+  const newOrder = await order.save();
+
+  res.status(201).json({
+    status: "success",
+    data: newOrder,
+  });
 });
--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const authController = require("../controllers/authController");
 
 const orderController = require("../controllers/orderController");
 
 const router = express.Router();
 
-router.route("/").get(orderController.getAllOrders);
+router
+  .route("/")
+  .get(authController.protect, orderController.getAllOrders)
+  .post(authController.protect, orderController.createOrder);
 
-router.route("/:id").get(orderController.getOrderById);
+router.route("/:id").get(authController.protect, orderController.getOrderById);
 
 module.exports = router;
