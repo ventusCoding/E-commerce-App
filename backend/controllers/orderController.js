fbd28e9d98f46ed325d918fbaff60393c4c23e4a
Anwer Baccar <anwer.baccar.pro@gmail.com>
2022-07-24 15:04:17 +0100

    add getOrderById

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -3,9 +3,41 @@ const APIFeatures = require("../utils/apiFeatures");
 const catchasync = require("../utils/catchAsync");
 const AppError = require("../utils/appError");
 
-exports.getAllOrders = catchasync(async (req, res, next) => {});
+exports.getAllOrders = catchasync(async (req, res, next) => {
+  const features = new APIFeatures(Order.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-exports.getOrderById = catchasync(async (req, res, next) => {});
+  const orders = await features.query;
+
+  if (orders.length > 0) {
+    res.status(200).json({
+      status: "success",
+      requestedAt: req.requestTime,
+      results: orders.length,
+      data: orders,
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      results: 0,
+      data: orders,
+      message: "No Data Found!",
+    });
+  }
+});
+
+exports.getOrderById = catchasync(async (req, res, next) => {
+    const order = await Order.findById(req.params.id);
+    
+    if (!order) {
+        return next(new AppError("No order found with that ID", 404));
+    }
+    
+    res.status(200).json({ status: "success", data: order });
+});
 
 exports.createOrder = catchasync(async (req, res, next) => {
   const {
